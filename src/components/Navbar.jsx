9210bfe80e1fec58723226a08caf19ac73a4fe2b
alvariezi <148828942+alvariezi@users.noreply.github.com>
2024-07-30 20:52:34 +0700

    menambah kan folder pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,24 @@
+import React from 'react';
+
 const Navbar = () => {
-    return ( 
-        <>
-        </>
-     );
-}
- 
+  return (
+    <nav className="bg-black w-[1140px] h-[81px] rounded-lg mx-auto flex items-center justify-between px-4">
+      <ul className="flex space-x-6 text-white">
+        <li><a href="#beranda" className="hover:underline">Beranda</a></li>
+        <li><a href="#biografi" className="hover:underline">Biografi</a></li>
+        <li><a href="#publikasi" className="hover:underline">Publikasi</a></li>
+        <li><a href="#rekam-jejak" className="hover:underline">Rekam Jejak</a></li>
+      </ul>
+      <div className="flex items-center space-x-6">
+        <img src="/logo.png" alt="Logo" className="h-10" />
+        <ul className="flex space-x-6 text-white">
+          <li><a href="#visi" className="hover:underline">Visi</a></li>
+          <li><a href="#testimonials" className="hover:underline">Testimonials</a></li>
+          <li><a href="#contact" className="hover:underline">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
