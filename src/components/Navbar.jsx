ce1076c43f996c1c71ec604088a535c980e0ebc3
IIQball <muhammadiqbl45@gmail.com>
2024-07-31 14:54:33 +0700

    tes update navbar icon path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { CaretDown, List, X } from 'phosphor-react';
+import React, { useState } from "react";
+import { CaretDown, List, X } from "phosphor-react";
 
 const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -20,8 +20,8 @@ const Navbar = () => {
   const handleScroll = (e, targetId) => {
     e.preventDefault();
 
-    if (window.location.pathname === '/') {
-      document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+    if (window.location.pathname === "/") {
+      document.getElementById(targetId).scrollIntoView({ behavior: "smooth" });
     }
   };
 
@@ -30,7 +30,7 @@ const Navbar = () => {
   };
 
   const navigateToLanding = () => {
-    window.location.href = '/';
+    window.location.href = "/";
   };
 
   return (
@@ -38,41 +38,89 @@ const Navbar = () => {
       <div className="hidden lg:flex items-center text-white w-full justify-between">
         <ul className="flex items-center flex-grow justify-between">
           <li className="ml-20 group">
-            <a href="/" onClick={(e) => { e.preventDefault(); navigateToLanding(); }} className="group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Beranda</a>
+            <a
+              href="/"
+              onClick={(e) => {
+                e.preventDefault();
+                navigateToLanding();
+              }}
+              className="group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300"
+            >
+              Beranda
+            </a>
           </li>
           <li className="mx-1 group">
-            <a href="#biografi" onClick={(e) => handleScroll(e, 'biografi')} className="group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Biografi</a>
+            <a
+              href="#biografi"
+              onClick={(e) => handleScroll(e, "biografi")}
+              className="group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300"
+            >
+              Biografi
+            </a>
           </li>
           <li className="mx-1 group">
-            <a href="#publikasi" onClick={(e) => handleScroll(e, 'publikasi')} className="group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Pojok Kandidat</a>
+            <a
+              href="#publikasi"
+              onClick={(e) => handleScroll(e, "publikasi")}
+              className="group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300"
+            >
+              Pojok Kandidat
+            </a>
           </li>
           <div className="flex-shrink-0 mx-20">
-            <img src="/public/assets/taufan/logo.svg" alt="Logo" className="h-10 mx-auto md:mx-0" />
+            <img
+              src="./public/assets/taufan/logo.svg"
+              alt="Logo"
+              className="h-10 mx-auto md:mx-0"
+            />
           </div>
           <li className="mx-1 group relative">
-            <button onClick={toggleDropdown} className="flex items-center group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300">
+            <button
+              onClick={toggleDropdown}
+              className="flex items-center group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300"
+            >
               Rekam Jejak
               <CaretDown
                 size={16}
-                className={`ml-1 transition-transform duration-300 ${isDropdownOpen ? 'rotate-180' : 'rotate-0'}`}
+                className={`ml-1 transition-transform duration-300 ${
+                  isDropdownOpen ? "rotate-180" : "rotate-0"
+                }`}
               />
             </button>
             {isDropdownOpen && (
               <ul className="absolute top-full left-0 mt-2 bg-black rounded shadow-lg">
-                <li className="px-4 py-2 hover:bg-[#D92D20] cursor-pointer" onClick={() => navigateToPage('/i_nyoman_sutjidra')}>
+                <li
+                  className="px-4 py-2 hover:bg-[#D92D20] cursor-pointer"
+                  onClick={() => navigateToPage("/i_nyoman_sutjidra")}
+                >
                   I Nyoman Sutjidra
                 </li>
-                <li className="px-4 py-2 hover:bg-[#D92D20] cursor-pointer" onClick={() => navigateToPage('/GedeSupriatna')}>
+                <li
+                  className="px-4 py-2 hover:bg-[#D92D20] cursor-pointer"
+                  onClick={() => navigateToPage("/GedeSupriatna")}
+                >
                   Gede Supriatna
                 </li>
               </ul>
             )}
           </li>
           <li className="mx-1 group">
-            <a href="#karier" onClick={(e) => handleScroll(e, 'karier')} className="group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Karier</a>
+            <a
+              href="#karier"
+              onClick={(e) => handleScroll(e, "karier")}
+              className="group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300"
+            >
+              Karier
+            </a>
           </li>
           <li className="mr-20 group">
-            <a href="#berita" onClick={(e) => handleScroll(e, 'berita')} className="group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Berita</a>
+            <a
+              href="#berita"
+              onClick={(e) => handleScroll(e, "berita")}
+              className="group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300"
+            >
+              Berita
+            </a>
           </li>
         </ul>
       </div>
@@ -82,44 +130,95 @@ const Navbar = () => {
           <List size={24} />
         </button>
       </div>
-      <div className={`fixed top-0 left-0 w-full h-screen bg-black text-white flex flex-col items-center transition-transform duration-300 ${isMenuOpen ? 'translate-y-0' : 'translate-y-[-100%]'}`}>
-        <button onClick={closeMenu} className="absolute top-4 right-4 text-white">
+      <div
+        className={`fixed top-0 left-0 w-full h-screen bg-black text-white flex flex-col items-center transition-transform duration-300 ${
+          isMenuOpen ? "translate-y-0" : "translate-y-[-100%]"
+        }`}
+      >
+        <button
+          onClick={closeMenu}
+          className="absolute top-4 right-4 text-white"
+        >
           <X size={24} />
         </button>
         <ul className="mt-16 w-full flex flex-col items-center">
           <li className="py-2">
-            <a href="/" onClick={(e) => { e.preventDefault(); navigateToLanding(); }} className="hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Beranda</a>
+            <a
+              href="/"
+              onClick={(e) => {
+                e.preventDefault();
+                navigateToLanding();
+              }}
+              className="hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300"
+            >
+              Beranda
+            </a>
           </li>
           <li className="py-2">
-            <a href="#biografi" onClick={(e) => handleScroll(e, 'biografi')} className="hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Biografi</a>
+            <a
+              href="#biografi"
+              onClick={(e) => handleScroll(e, "biografi")}
+              className="hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300"
+            >
+              Biografi
+            </a>
           </li>
           <li className="py-2">
-            <a href="#publikasi" onClick={(e) => handleScroll(e, 'publikasi')} className="hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Pojok Kandidat</a>
+            <a
+              href="#publikasi"
+              onClick={(e) => handleScroll(e, "publikasi")}
+              className="hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300"
+            >
+              Pojok Kandidat
+            </a>
           </li>
           <li className="py-2 relative">
-            <button onClick={toggleDropdown} className="flex items-center hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300">
+            <button
+              onClick={toggleDropdown}
+              className="flex items-center hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300"
+            >
               Rekam Jejak
               <CaretDown
                 size={16}
-                className={`ml-1 transition-transform duration-300 ${isDropdownOpen ? 'rotate-180' : 'rotate-0'}`}
+                className={`ml-1 transition-transform duration-300 ${
+                  isDropdownOpen ? "rotate-180" : "rotate-0"
+                }`}
               />
             </button>
             {isDropdownOpen && (
               <ul className="absolute top-full left-0 mt-2 bg-black rounded shadow-lg ">
-                <li className="px-4 py-2 hover:bg-[#D92D20] cursor-pointer" onClick={() => navigateToPage('/i_nyoman_sutjidra')}>
+                <li
+                  className="px-4 py-2 hover:bg-[#D92D20] cursor-pointer"
+                  onClick={() => navigateToPage("/i_nyoman_sutjidra")}
+                >
                   I Nyoman Sutjidra
                 </li>
-                <li className="px-4 py-2 hover:bg-[#D92D20] cursor-pointer" onClick={() => navigateToPage('/GedeSupriatna')}>
+                <li
+                  className="px-4 py-2 hover:bg-[#D92D20] cursor-pointer"
+                  onClick={() => navigateToPage("/GedeSupriatna")}
+                >
                   Gede Supriatna
                 </li>
               </ul>
             )}
           </li>
           <li className="py-2">
-            <a href="#karier" onClick={(e) => handleScroll(e, 'karier')} className="hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Karier</a>
+            <a
+              href="#karier"
+              onClick={(e) => handleScroll(e, "karier")}
+              className="hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300"
+            >
+              Karier
+            </a>
           </li>
           <li className="py-2">
-            <a href="#berita" onClick={(e) => handleScroll(e, 'berita')} className="hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Berita</a>
+            <a
+              href="#berita"
+              onClick={(e) => handleScroll(e, "berita")}
+              className="hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300"
+            >
+              Berita
+            </a>
           </li>
         </ul>
       </div>
