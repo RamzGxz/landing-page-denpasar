c717b6bc0ca7241f5c709ef6e09a68efba38a928
Nafiskurniatul <92704434+Nafiskurniatul@users.noreply.github.com>
2024-07-31 11:46:54 +0700

    menambah gambar dan component berita
    menambahkan gambar pada asset dan component berita

--- src/pages/LandingPages.jsx
+++ src/pages/LandingPages.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import Berita from "../components/Berita";
 
 const LandingPages = () => {
     return ( 
         <>
         <Navbar />
+        <Berita/>
         </>
      );
 }
