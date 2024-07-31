c7c98399a4d4952c75f684514f34dd3305f38720
alvariezi <148828942+alvariezi@users.noreply.github.com>
2024-07-31 10:24:52 +0700

    revisi navbar dan nama calon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,108 @@
-import React from 'react';
-import { Quotes } from 'phosphor-react'; 
+import React, { useState } from 'react';
+import { CaretDown, List, X } from 'phosphor-react';
 
-const Hero = () => {
+const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
-    <div className="mx-auto w-full flex flex-col items-center justify-center mt-5">
-      <div className="text-center">
-        <img 
-          src="/assets/taufan/hello.svg" 
-          alt="Decorative" 
-          className="w-[129px] h-[64px] mx-auto mt-4 mb-2"
-        />
-        <div className="text-[30px] font-semibold lg:text-[70px]">
-          I'm
-          <span className="text-[#D92D20]"> Gede</span> Supriatna
+    <nav className="bg-black sticky top-5 h-[50px] rounded-xl w-full lg:max-w-screen-xl lg:mx-auto lg:h-[75px] flex items-center z-50 lg:top-5">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-14 lg:max-w-full">
+        <div className="hidden lg:flex items-center text-white flex-grow justify-between">
+          <ul className="flex items-center flex-grow justify-between">
+            <li className="ml-20 group">
+              <a href="#beranda" className="group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Beranda</a>
+            </li>
+            <li className="mx-1 group">
+              <a href="#biografi" className="group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Biografi</a>
+            </li>
+            <li className="mx-1 group">
+              <a href="#publikasi" className="group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Pojok Kandidat</a>
+            </li>
+            <div className="flex-shrink-0 mx-20">
+              <img src="/public/assets/taufan/logo.svg" alt="Logo" className="h-10 mx-auto md:mx-0" />
+            </div>
+            <li className="mx-1 group relative">
+              <button onClick={toggleDropdown} className="flex items-center group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300">
+                Rekam Jejak
+                <CaretDown 
+                  size={16} 
+                  className={`ml-1 transition-transform duration-300 ${isDropdownOpen ? 'rotate-180' : 'rotate-0'}`} 
+                />
+              </button>
+              {isDropdownOpen && (
+                <ul className="absolute top-full left-0 mt-2 bg-black rounded shadow-lg">
+                  <li className="px-4 py-2 hover:bg-[#D92D20]"><a href="/i_nyoman_sutjidra">I Nyoman Sutjidra</a></li>
+                  <li className="px-4 py-2 hover:bg-[#D92D20]"><a href="/GedeSupritana">Gede Supriatna</a></li>
+                </ul>
+              )}
+            </li>
+            <li className="mx-1 group">
+              <a href="#karier" className="group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Karier</a>
+            </li>
+            <li className="mr-20 group">
+              <a href="#berita" className="group-hover:bg-[#D92D20] group-hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Berita</a>
+            </li>
+          </ul>
         </div>
-        <div className="text-[30px] font-semibold lg:text-[70px]">
-          I 
-          <span className="text-[#D92D20]"> Nyoman</span> Sutdijra
+        <div className="flex lg:hidden items-center justify-between w-full">
+          <img src="/public/assets/taufan/logo.svg" alt="Logo" className="h-10" />
+          <button onClick={toggleMenu} className="text-white ml-4">
+            <List size={24} />
+          </button>
+        </div>
+        <div className={`fixed top-0 left-0 w-full h-screen bg-black text-white flex flex-col items-center transition-transform duration-300 ${isMenuOpen ? 'translate-y-0' : 'translate-y-[-100%]'}`}>
+          <button onClick={closeMenu} className="absolute top-4 right-4 text-white">
+            <X size={24} />
+          </button>
+          <ul className="mt-16 w-full flex flex-col items-center">
+            <li className="py-2">
+              <a href="#beranda" className="hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Beranda</a>
+            </li>
+            <li className="py-2">
+              <a href="#biografi" className="hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Biografi</a>
+            </li>
+            <li className="py-2">
+              <a href="#publikasi" className="hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Pojok Kandidat</a>
+            </li>
+            <li className="py-2 relative">
+              <button onClick={toggleDropdown} className="flex items-center hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300">
+                Rekam Jejak
+                <CaretDown 
+                  size={16} 
+                  className={`ml-1 transition-transform duration-300 ${isDropdownOpen ? 'rotate-180' : 'rotate-0'}`} 
+                />
+              </button>
+              {isDropdownOpen && (
+                <ul className="absolute top-full left-0 mt-2 bg-black rounded shadow-lg">
+                   <li className="px-4 py-2 hover:bg-[#D92D20]"><a href="/i_nyoman_sutjidra">I Nyoman Sutjidra</a></li>
+                   <li className="px-4 py-2 hover:bg-[#D92D20]"><a href="/GedeSupritana">Gede Supriatna</a></li>
+                </ul>
+              )}
+            </li>
+            <li className="py-2">
+              <a href="#karier" className="hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Karier</a>
+            </li>
+            <li className="py-2">
+              <a href="#berita" className="hover:bg-[#D92D20] hover:text-white px-3 py-2 rounded-[60px] transition duration-300">Berita</a>
+            </li>
+          </ul>
         </div>
       </div>
-      <div className="flex w-full max-w-6xl justify-center items-center">
-        <div className="hidden lg:flex relative left-0 w-64 p-6 bg-white shadow-lg rounded-lg text-center flex-col items-center">
-          <Quotes size={24} className="text-gray-500 mb-4" />
-          <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <img 
-          src="/assets/taufan/hero.png" 
-          alt="Hero" 
-          className="w-full mx-auto lg:w-[700px] lg:h-[400px]"
-        />
-        <div className="hidden lg:flex relative right-0 w-64 p-6 bg-white shadow-lg rounded-lg text-center flex-col items-center">
-          <Quotes size={24} className="text-gray-500 mb-4" />
-          <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-      </div>
-    </div>
+    </nav>
   );
-}
+};
 
-export default Hero;
+export default Navbar;
