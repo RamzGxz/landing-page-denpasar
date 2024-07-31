b9f99af329f3354918e9781d6f2039a4dbc2dc1d
zittawardani <zittawardani@gmail.com>
2024-07-31 11:10:07 +0700

    menambahkan pages DetailGede, menambahkan background.png dan gedesupriatna.png di assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPages from './pages/LandingPages';
+import DetailGede from './pages/DetailGede';
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
     <Route path="/" element={<LandingPages />} />
+    <Route path="/GedeSupriatna" element={<DetailGede/>} />
     </Routes>
     </BrowserRouter>
   );
