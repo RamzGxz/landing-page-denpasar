ccb1679402e9b8ab50116a4007206e3ab686a1e7
EkallFH <mhmmdfkrihaikall@gmail.com>
2024-07-31 10:38:59 +0700

    create page detail calon

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPages from './pages/LandingPages';
+import DetailNyoman from './pages/DetailNyoman';
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<LandingPages />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<LandingPages />} />
+        <Route path="/i_nyoman_sutjidra" element={<DetailNyoman />} />
+      </Routes>
     </BrowserRouter>
   );
 };
