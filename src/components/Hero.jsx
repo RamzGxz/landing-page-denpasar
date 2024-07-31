12c3b703f96395990f4a4ad5cf788654cfb83e18
alvariezi <148828942+alvariezi@users.noreply.github.com>
2024-07-31 11:03:45 +0700

    revisi hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,6 @@ const Hero = () => {
           className="w-[129px] h-[64px] mx-auto mt-24 lg:mt-40 mb-2"
         />
         <div className="text-[30px] font-semibold lg:text-[70px]">
-          I'm
           <span className="text-[#D92D20]"> Gede</span> Supriatna
         </div>
         <div className="text-[30px] font-semibold lg:text-[70px]">
@@ -20,7 +19,7 @@ const Hero = () => {
         </div>
       </div>
       <div className="flex w-full max-w-6xl justify-center items-center">
-        <div className="hidden lg:flex relative left-0 w-64 p-6 bg-white shadow-lg rounded-lg text-center flex-col items-center">
+        <div className="hidden lg:flex relative left-2 w-64 p-6 bg-white shadow-lg rounded-lg text-center flex-col items-center">
           <Quotes size={24} className="text-gray-500 mb-4" />
           <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
@@ -29,7 +28,7 @@ const Hero = () => {
           alt="Hero" 
           className="w-full mx-auto lg:w-[700px] lg:h-[400px]"
         />
-        <div className="hidden lg:flex relative right-0 w-64 p-6 bg-white shadow-lg rounded-lg text-center flex-col items-center">
+        <div className="hidden lg:flex relative right-2 w-64 p-6 bg-white shadow-lg rounded-lg text-center flex-col items-center">
           <Quotes size={24} className="text-gray-500 mb-4" />
           <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
